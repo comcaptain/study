7edaaaf25844902594f4d533aa94b9ea922a3aa5
孙国强 <htmlcaptain@gmail.com>
2023-12-13 13:37:12 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# Check if at least one file is provided
-if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 file1 [file2 ...]"
+# Check if input file is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 filename"
     exit 1
 fi
 
-# Merge the files
-awk '
+input_file="$1"
+base_name="${input_file%.*}"
+
+# Initialize the first output file for content before the first "# Built with:"
+filecount=0
+outfile="${base_name}-${filecount}.txt"
+
+# Split the file using awk
+awk -v base_name="$base_name" -v outfile="$outfile" '
+    /^# Built with:$/ {
+        close(outfile)
+        outfile = base_name "-" ++filecount ".txt"
+    }
     {
-        # Append lines from each file
-        a[FNR] = (FNR in a ? a[FNR] OFS : "") $0
+        print > outfile
     }
-    END {
-        # Output the combined lines
-        for (i = 1; i <= FNR; i++) {
-            print a[i]
-        }
-    }
-' "$@"
+' "$input_file"
